a53e409ff95c2156e01b7bf671bdc22b7995be01
Nikki A <nikki.apostolakis@gmail.com>
2018-08-12 23:33:38 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,9 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists
+    names = []
+    self.artists.each do |artist|
+      names << artist.names
+    end
   end
 end
